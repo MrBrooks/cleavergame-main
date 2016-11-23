9790003b7c014fad57f4dd278483173f43faba61
Denis Kulikov <altingfest@gmail.com>
2016-11-23 19:06:32 +0300

    add menu btn

--- js/custom.js
+++ js/custom.js
@@ -10,4 +10,17 @@ $(document).ready(function() {
       dots: false,
       navText: ['<div class="svg-sprite--arrow"></div>','<div class="svg-sprite--arrow"></div>']
     });
+  var menu = new MenuBtn();
 });
+
+function MenuBtn(){
+  var btn = $('#cg-menu-btn'),
+      drop = $('#cg-menu');
+
+  btn.on('click', toggle);
+
+  function toggle(){
+    btn.toggleClass('active');
+    drop.slideToggle(300);
+  }
+}
