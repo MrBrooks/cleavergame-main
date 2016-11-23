bd0e3e3371304f301d11ef0a09e85d56ddc5c0cb
Denis Kulikov <altingfest@gmail.com>
2016-11-23 14:56:34 +0300

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+/**************************************/
+/* Custom JavaScript files supervisor */
+/**************************************/
+
+$(document).ready(function() {
+  $('#cg-comment__slider').owlCarousel({
+      items: 1,
+      loop: true,
+      nav: true,
+      dots: false,
+      navText: ['<div class="svg-sprite--arrow"></div>','<div class="svg-sprite--arrow"></div>']
+    });
+});
